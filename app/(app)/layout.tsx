bc29d4cad77c31f805a961a5073e4e64d384fc8c
babono <babono@hotmail.com>
2025-09-17 22:47:54 +0700

    setup payloadcms on supabase

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import Navigation from "../components/Navigation";
+import Navigation from "./components/Navigation";
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
